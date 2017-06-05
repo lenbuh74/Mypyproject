4b32629efbc38e950d146640dfa78a28c2bfba5d
lenbuh74 <lenbuh74@mail.ru>
2017-06-05 20:05:32 +0300

    Add files via upload

---
+++ registration/urls.py
@@ -0,0 +1,29 @@
+"""
+Backwards-compatible URLconf for existing django-registration
+installs; this allows the standard ``include('registration.urls')`` to
+continue working, but that usage is deprecated and will be removed in
+a future release.  For new installs, use
+``include('registration.backends.model_activation.urls')``.
+
+"""
+
+import warnings
+
+from registration.backends.model_activation import urls as model_urls
+
+
+warnings.warn(
+    "include('registration.urls') is deprecated; use "
+    "include('registration.backends.model_activation.urls') instead.",
+
+    DeprecationWarning
+)
+from registration.views import login, logout
+
+urlpatterns = (
+    # существующие шаблоны располагаются здесь...
+    (r'^accounts/login/$',  login),
+    (r'^accounts/logout/$', logout),
+)
+
+urlpatterns = model_urls.urlpatterns
