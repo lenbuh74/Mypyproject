3505f926ef4d7273ca412fa46b0dd9022cb919d7
lenbuh74 <lenbuh74@mail.ru>
2017-06-05 19:56:07 +0300

    Add files via upload

---
+++ myshop/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^', include('shop.urls', namespace='shop')),
+    url(r'^order/', include('orders.urls', namespace='orders')),
+    url(r'^paypal/', include('paypal.standard.ipn.urls')),
+    url(r'^payment/', include('payment.urls', namespace='payment')),
+    url(r'^cupons/', include('cupons.urls', namespace='cupon')),
+    url(r'^accounts/', include('registration.urls')),
+    url(r'^form/', include('contact.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
